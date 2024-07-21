412c04ed9d5f81c8b161b8334101fb817f0f428b
Daniyar.Serikov <daniyar.serikov@uco.kz>
2024-07-21 15:08:15 +0500

    fix

--- src/telegram.d.ts
+++ src/telegram.d.ts
@@ -1,21 +1,21 @@
 // src/telegram.d.ts
 interface TelegramUser {
-    id: number;
-    first_name: string;
-    last_name?: string;
-    username?: string;
-  }
-  
-  interface TelegramWebApp {
-    initDataUnsafe?: {
-      user?: TelegramUser;
-    };
-    ready: () => void;
-  }
-  
-  interface Window {
-    Telegram?: {
-      WebApp: TelegramWebApp;
-    };
-  }
-  
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  photo_url?: string; // Add this line
+}
+
+interface TelegramWebApp {
+  initDataUnsafe?: {
+    user?: TelegramUser;
+  };
+  ready: () => void;
+}
+
+interface Window {
+  Telegram?: {
+    WebApp: TelegramWebApp;
+  };
+}
