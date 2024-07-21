877eca3520ca34f153b5bdd390b761e307c240d3
Daniyar.Serikov <daniyar.serikov@uco.kz>
2024-07-21 12:21:39 +0500

    ds

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000,
-    strictPort: true, // Ensures Vite uses the specified port and fails if it's not available
-  },
-  preview: {
-    port: process.env.PORT || 5000,
-    strictPort: true,
-  },
 })
